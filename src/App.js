77bcf63dead0a7552da8270e5aee0d100499ca5e
kaitlynsuzuki <kaitlynksuzuki@gmail.com>
2021-02-19 13:54:05 -0800

    Added the Emissions Component
    Calculates the emissions and displays it depending on the distance parameter

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Main from './components/Main';
 import MapSection from './components/Map';
 import Footer from './components/Footer';
+import Emissions from './components/Emissions';
 
 const location = {
   address: '4255 Campus Dr A150, Irvine, CA 92612',
@@ -16,6 +17,7 @@ function App() {
     <div className="App">
         <Header />
         <Main />
+        <Emissions distance={100}/>
         <MapSection location={location} zoomLevel={17} />
         <Footer />
     </div>
---
+++ src/components/Emissions.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+const request = require('request');
+console.log("hey" + request);
+
+  
+class Emissions extends Component {
+    constructor(props) {
+        super(props);
+        var gal = (this.props.distance/25.0);
+
+        this.emissions = gal * 8.887;
+    };
+    render() { 
+        return ( 
+            <div className="Emissions">
+                
+                <h1>{this.emissions}</h1>
+                <p>Based on a 25mpg fuel efficiency</p>
+                <p> According to the <a href="https://www.epa.gov/energy/greenhouse-gases-equivalencies-calculator-calculations-and-references">Greenhouse Gases Equivalencies Calculator</a> by the EPA</p>
+            </div>
+         );
+    }
+}
+ 
+export default Emissions;
